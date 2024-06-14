fe9077e8ab4d86400870bc1d8b73d69f9a60d801
Alessandro Tirelli <64091907+tirellovic@users.noreply.github.com>
2024-06-14 09:55:50 +0200

    Small bug fix

--- frontend/src/pages/HomeUtente.tsx
+++ frontend/src/pages/HomeUtente.tsx
@@ -65,7 +65,7 @@ const HomeUtente: React.FC<HomeUtenteProps> = ({ token }) => {
         <div className="bg-gray-50 dark:bg-gray-900 min-h-screen">
             <Navbar />
             <div className='max-w-screen-xl mx-auto'>
-                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-3xl p-10">Garage di <span className='italic' >{id}</span></h1>
+                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-3xl p-10">Garage di <span className='italic' >{email}</span></h1>
                 <CardList cards={vehicles} />
             </div>
         </div>
